8416acf9ea0b01d0bd202ace881a974bd16334ad
baskoro aji susetyo Koko <baskoroajisusetyo@gmail.com>
2019-11-26 11:52:04 +0700

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/HDD-A/Development/Python/django/fullstack bootcamp/lesson1/part4/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
