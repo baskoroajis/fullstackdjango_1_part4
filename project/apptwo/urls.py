b1e7de9e2fe1de8790d824898368c9b19bbc2558
baskoro aji susetyo Koko <baskoroajisusetyo@gmail.com>
2019-11-28 15:11:21 +0700

    level three (form input in post method)

--- project/apptwo/urls.py
+++ project/apptwo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from apptwo import views
 
 urlpatterns = [
-    path('', views.getUser, name='getUser')
+    path('', views.getUser, name='get_user'),
+    path('register',views.register_user, name='register_user')
 ]
--- project/apptwo/views.py
+++ project/apptwo/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from apptwo.models import User
 from django.http import HttpResponse
+from apptwo.forms import  NewUserForm
 # Create your views here.
 
 def getUser(request):
     user_list = User.objects.order_by('first_name')
     data = {'user_data' : user_list}
     return  render(request, 'apptwo/User.Html', context=data)
+
+def register_user(request):
+    form = NewUserForm()
+
+    if (request.method == 'POST'):
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponse('sukses register new user')
+
+    return render(request, 'apptwo/register_form.html', context={'form' : form})
 
